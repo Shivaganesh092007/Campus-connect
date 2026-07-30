c8e4cb4f0b1430c72362a7fc2f7ea8aa5648108f
Shivaganesh092007 <shivaganesh4318@gmail.com>
2026-07-31 00:46:30 +0530

    Added updateQuestion route definition in question.routes.js

--- Backend/src/routes/question.routes.js
+++ Backend/src/routes/question.routes.js
@@ -6,12 +6,14 @@ import {
     getQuestionById,
     toggleQuestionUpvote,
     markQuestionSolved,
+    updateQuestion,
 } from "../controllers/question.controller.js";
 import answerRouter from "./answer.routes.js";
 
 const router = Router();
 
 router.route("/").post(verifyJWT, createQuestion);
+router.route("/:id").patch(verifyJWT, updateQuestionQuestion);
 router.route("/").get(getAllQuestions);
 router.route("/:id").get(getQuestionById);
 router.route("/:id/upvote").post(verifyJWT, toggleQuestionUpvote);
